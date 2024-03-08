af9b9a3e6db224611b1b2a802dfacb4f8110ee43
brucewaynepioneer <158065590+brucewaynepioneer@users.noreply.github.com>
2024-03-08 16:53:42 +0530

    Update basic.py

--- StringGenBot/basic.py
+++ StringGenBot/basic.py
@@ -6,15 +6,15 @@ from pyrogram.types import InlineKeyboardMarkup, Message
 def filter(cmd: str):
     return filters.private & filters.incoming & filters.command(cmd)
 
-
+gif = "start.mp4"
 # Start Message
 @Client.on_message(filter("start"))
 async def start(bot: Client, msg: Message):
     user = await bot.get_me()
     mention = user.mention
-    await bot.send_message(
+    await bot.send_animation(
         msg.chat.id,
-        Data.START.format(msg.from_user.mention, mention),
-        disable_web_page_preview=True,
+        animation=gif,
+        caption=f"""Welcome to String Bot!""",
         reply_markup=InlineKeyboardMarkup(Data.buttons)
     )
